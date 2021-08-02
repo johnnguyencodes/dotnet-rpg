4f6bb4f315e5fcf0e1bdb990e0bb0c8bc18e2c25
johnnguyenzymo <jnguyen@zymoresearch.com>
2021-08-02 09:17:15 -0700

    Asynchronous Calls

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,19 +1,40 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using dotnet_rpg.Models;
+using dotnet_rpg.Services.CharacterService;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_rpg.Controllers
 {
 
-    [ApiController]
-    [Route("[controller]")]
-    public class CharacterController : ControllerBase
-    {
-        private static Character knight = new Character();
+  [ApiController]
+  [Route("[controller]")]
+  public class CharacterController : ControllerBase
+  {
+    private readonly ICharacterService _characterService;
 
-        [HttpGet]
-        public ActionResult<Character> Get()
-        {
-            return Ok(knight);
-        }
+    public CharacterController(ICharacterService characterService)
+    {
+      _characterService = characterService;
     }
+
+    [HttpGet("GetAll")]
+    public async Task<ActionResult<List<Character>>> Get()
+    {
+      return Ok(await _characterService.GetAllCharacters());
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Character>> GetSingle(int id)
+    {
+      return Ok(await _characterService.GetCharacterById(id));
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<List<Character>>> AddCharacter(Character newCharacter)
+    {
+      return Ok(await _characterService.AddCharacter(newCharacter));
+    }
+  }
 }
